1b65dfe72e8ccfdd08a3a65eba03fa3143c754ae
Roshini G <64271786+Roshini2000@users.noreply.github.com>
2022-11-07 10:29:21 +0530

    Create Home.py

---
+++ mini project/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = 'Home Page'
+)
+
+st.title("Expert Financial Planner")
+st.sidebar.success("Select a Page below")
+st.header('Disclaimer')
+st.subheader('All information, illustrations, calculations and projections shown in this project are intented to be used as a tool to facilitate the financial planning exercise and is a working document. It SHOULD NOT BE CONSIDERED AS FINANCIAL ADVICE in any way and should not be acted on as such') 	
+st.text('')
